5e2301ae3130cde59c5e54de1c7bb386ac6d6a37
georgekuchuryan <110783800+georgekuchuryan@users.noreply.github.com>
2022-10-26 22:14:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/footer/footer";
+import MainPage from "./components/MainPage/MainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MainPage.module.scss';
+
+const MainPage = () => (
+  <div className={styles.MainPage}>
+    MainPage Component
+  </div>
+);
+
+MainPage.propTypes = {};
+
+MainPage.defaultProps = {};
+
+export default MainPage;
